00ac7a41673e209e3cead8cef05d34e928b64ee7
Srijabanerjee <srijab022@gmail.com>
2021-12-12 12:46:59 +0530

    myportfolio

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+/*about section tabs*/
+(() =>{
+        const aboutSection = document.querySelector(".about-section"),
+        tabsContainer = document.querySelector(".about-tabs");
+
+        tabsContainer.addEventListener("click", (event) => {
+          if(event.target.classList.contains("tab-item") &&
+             !event.target.classList.contains("active")){
+               const target = event.target.getAttribute("data-target");
+               //deactivate exixting active 'tab-item'
+               tabsContainer.querySelector(".active").classList.remove("outer-shadow","active");
+               event.target.classList.add("active","outer-shadow");
+          aboutSection.querySelector(".tab-content.active").classList.remove("active");
+          aboutSection.querySelector(target).classList.add("active");
+             }
+        })
+})();
